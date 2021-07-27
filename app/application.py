46dfb3a5d73f893f08b0bc5fe3598bdbc12602dd
Saad Farooq <shloth@Saads-MBP.fios-router.home>
2021-07-27 12:32:21 -0400

    nginx regex cleanup attempt 22

--- app/application.py
+++ app/application.py
@@ -11,7 +11,7 @@ os_info = platform.platform()
 nginx_v = subprocess.run(
     ["nginx", "-v"], stdout=PIPE
 )
-nginx_raw_v = re.sub('\D', '', nginx_v)
+nginx_raw_v = re.sub('\D', '', "f{nginx_v}")
 print(nginx_raw_v)
 
 def nginx_install():
