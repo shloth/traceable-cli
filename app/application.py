1964e4e75c0bacc331cde3202a442dfe09cbe696
Saad Farooq <shloth@Saads-MBP.fios-router.home>
2021-07-28 12:18:10 -0400

    cp *.so files to nginx modpath attempt 1

--- app/application.py
+++ app/application.py
@@ -22,7 +22,7 @@ def nginx_install():
 
     nginx_modpath = re.search(r'(?<=--modules-path=)[^\s]*',nginx_details)
     #print(nginx_details)
-    print(nginx_modpath.group(0))
+    path = nginx_modpath.group(0)
 
     # Find if --with-compat flag is present
     if "--with-compat" in nginx_details:
@@ -59,9 +59,10 @@ def nginx_install():
     r = requests.get(url, allow_redirects=True)
     open('{}-x86_64-nginx-{}-ngx_http_module.so.tgz'.format(os, nginx_raw_v), 'wb').write(r.content)
     file_name = '{}-x86_64-nginx-{}-ngx_http_module.so.tgz'.format(os, nginx_raw_v)
-    extracted_files = subprocess.getoutput(["tar", "-xvzf", file_name])
-    
-    #subprocess.run(["mv", ])
+    extract_files = subprocess.getoutput(["tar", "-xvzf", file_name])
+    # Move extracted .so files to nginx modules_path
+    subprocess.run(["cp", "*.so", path])
+    subprocess.run("ls", "-la", path)
 #def platform_install():
 
 
