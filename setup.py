6738e01b323cf7c7b341dc24ef964c518b90d595
Saad Farooq <shloth@Saads-MBP.fios-router.home>
2021-08-16 17:31:26 -0400

    pypi prep #5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 setup(
     name = 'traceable-cli',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Saad Farooq',
     author_email = 'saad@traceable.ai',
     license = 'MIT',
@@ -16,9 +16,9 @@ setup(
     py_modules = ['traceable_cli', 'app'],
     packages = find_packages(),
     install_requires = [requirements],
-    python_requires='>=3',
+    python_requires='>=3.6',
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",      
         "Topic :: Software Development :: Build Tools",
